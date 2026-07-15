82bd15ea612cbd50e087e8939bb76eda65150da9
Pranav Phiske <phiskepranav@gmail.com>
2026-07-15 20:42:24 +0530

    Refactor merge sort implementation with comments
    Fixed syntax errors and added comments for clarity.

--- '''we are learning merge sort'''.c
+++ '''we are learning merge sort'''.c
@@ -1,4 +1,4 @@
-'''we are learning merge sort'''
+/*we are learning merge sort*/
 #include<stdio.h>
 void divide(int a[], int small, int large)
 {
@@ -6,16 +6,16 @@ void divide(int a[], int small, int large)
     if(small<large)
     {
         mid=(large+small)/2;
-        divide(a,small,mid);'''recursion, divided array into 2 parts'''
-        divide(a,mid+1,large);'''second part of array'''
-        merge(a,small,mid,large);'''function call'''
+        divide(a,small,mid);/*recursion, divided array into 2 parts*/
+        divide(a,mid+1,large);/*second part of array*/
+        merge(a,small,mid,large);/*function call*/
     }
 }
-void merge(a,small,mid,large)'''function defintion'''
+void merge(a,small,mid,large)/*function defintion*/
 {
     int x,y,temp[100],z=0;
-    x=small;'''defined the start of array in x'''
-    y=mid+1;'''considered first half of array''' 
+    x=small;/*defined the start of array in x*/
+    y=mid+1;/*considered first half of array*/ 
     while(x<=mid&&y<=large)
     {
         if(a[x]<a[y])
