25ba2fff60a30a08229c4c2b8c5854c025aa5296
Pranav Phiske <phiskepranav@gmail.com>
2026-07-25 14:44:25 +0530

    Add files via upload

--- Problem 8.c
+++ Problem 8.c
@@ -4,7 +4,6 @@ Assume that k is always less than or equal to the length of the Linked List.
 Eg:- Input: LinkedList: 1 -> 2 -> 3 -> 4 -> 5 -> 6, k = 2
 Output: 1 -> 3 -> 5 
 Explanation: After removing every 2nd node of the linked list, the resultant linked list will be: 1 -> 3 -> 5 .*/
-/*DEBUGGED BY MYSELF WITHOUT USING CLAUDE AI*/
 #include<stdio.h>
 #include<stdlib.h>
 #include<malloc.h>
@@ -22,7 +21,7 @@ struct node inputll(struct node *next){/*ERROR:- expected identifier, Same mista
 struct node altll(struct node *ptr, struct node *preptr){
     struct node *ll2, *ll1, *i;
     ptr=0, preptr=ptr-1;
-    for(i=ptr;i->next!=NULL;i->next){/*ERROR:- invalid argument ->, PATCHED*/
+    for(i=ptr;i->next!=NULL;i->next=ptr->next){/*ERROR:- invalid argument ->*/
         preptr=ptr;
         ptr=ptr->next;
         preptr->next=ptr->next;
