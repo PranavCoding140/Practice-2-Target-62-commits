a6f338ddfcb798b29951e3aa4cdb9e8f40727c46
Pranav Phiske <phiskepranav@gmail.com>
2026-07-25 13:15:46 +0530

    Fix errors in linked list node removal implementation

--- Problem 8.c
+++ Problem 8.c
@@ -21,7 +21,7 @@ struct node inputll(struct node *next){/*ERROR:- expected identifier, Same mista
 struct node altll(struct node *ptr, struct node *preptr){
     struct node *ll2, *ll1, *i;
     ptr=0, preptr=ptr-1;
-    for(i=ptr;i->next!=NULL;i->next){/*ERROR:- invalid argument ->*/
+    for(i=ptr;i->next!=NULL;i->next){/*ERROR:- invalid argument ->, PATCHED*/
         preptr=ptr;
         ptr=ptr->next;
         preptr->next=ptr->next;
