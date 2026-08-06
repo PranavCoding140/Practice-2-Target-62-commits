22206bec10d7704d968e2270acae858b61a8ae1d
Pranav Phiske <phiskepranav@gmail.com>
2026-08-06 18:58:10 +0530

    Fix findkth function and improve inorder traversal
    Refactor the findkth function to accept an integer k and fix the inorder function to properly traverse the tree.

--- Problem 13.c
+++ Problem 13.c
@@ -11,16 +11,17 @@ struct node *maketree(struct node *tree){
     tree=(struct node*)malloc(sizeof(struct node));
     printf("Enter the values of tree:");
     scanf("%d", data);
-    
+    tree->left=NULL;
+    tree->right=NULL;
 }
-struct node *findkth(struct node *tree, struct node* left, struct node* right){/*which traversal technique to be used? answer: inorder*/
-    int k;
+struct node *findkth(struct node *tree, int k){/*which traversal technique to be used? answer: inorder*/
+    int count=0;
     struct node *ptr;
     printf("Enter the kth element to be found:");
     scanf("%d", k);
-    void inorder(struct node *tree){
+    void inorder(struct node *tree, int k){
     if(tree!=NULL){
-        while(tree->left!=k||tree->right!=k){/*WARNING:- COMPARISON BETWEEN POINTER AND INTEGER*/
+        while(tree->left!=NULL||tree->right!=NULL){/*WARNING:- COMPARISON BETWEEN POINTER AND INTEGER*/
         inorder(tree->left);
         printf("%d", tree->data);
         inorder(tree->right);
@@ -30,6 +31,6 @@ struct node *findkth(struct node *tree, struct node* left, struct node* right){/
 int main(){
     struct node *tree;
     struct node *maketree(struct node *tree);
-    struct node *findkth(struct node *tree, struct node* left, struct node* right);
+    struct node *findkth(struct node *tree, int k);
     return 0;
 }
